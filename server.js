1df930bd41a9720e40bd6563e5bcf7cbcf51b708
Andrea Scarsetto <andrea.scarsetto@gmail.com>
2020-05-10 14:56:59 +0200

    .

--- server.js
+++ server.js
@@ -1,26 +1,16 @@
 const CDP = require('chrome-remote-interface');
+const express = require("express");
+var app = express();
 
-async function example() {
+async function changeDisplayStyle(style) {
     let client;
     try {
-        // connect to endpoint
         client = await CDP();
-        // extract domains
         const {Network, Page,Runtime} = client;
-        // setup handlers
-        Network.requestWillBeSent((params) => {
-            console.log(params.request.url);
-        });
-        // enable events then start!
-     /*   await Network.enable();
-        await Page.enable();
-        await Page.navigate({url: 'https://WWW.GITHUB.COM'});
-        await Page.loadEventFired(); */
-        await Runtime.evaluate({expression: "document.getElementById('rss').style.display = 'none'"})
-       await Runtime.evaluate({expression: "document.getElementById('weather').style.display = 'none'"})
-       await Runtime.evaluate({expression: "document.getElementById('calendar').style.display = 'none'"})
-    //    await Runtime.evaluate({expression: "document.getElementById('weather').style.display = 'block'"})
-        //await Page.console.
+        await Runtime.enable();
+        await Runtime.evaluate({expression: "document.getElementsByTagName('header')[0].style.display = '"+style+"'"})
+        await Runtime.evaluate({expression: "document.getElementsByTagName('footer')[0].style.display = '"+style+"'"})
+        await Runtime.evaluate({expression: "document.getElementById('rss').style.display = '"+style+"'"})
     } catch (err) {
         console.error(err);
     } finally {
@@ -30,4 +20,12 @@ async function example() {
     }
 }
 
-example();
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+   });
+
+app.get("/changeStyle/:style", (req, res, next) => {
+    changeDisplayStyle(req.params.style).then(() => {
+        res.send("OK")
+      });
+});
